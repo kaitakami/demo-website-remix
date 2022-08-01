12a3fb0f201ab72c03fe94e26698767ca8a30ec2
Kai Takami <kaitakami@protonmail.com>
2022-08-01 22:22:10 +0900

    6th blog

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,19 +1,32 @@
-import { Link } from 'remix';
+import { Link } from "remix";
 
 export default function BlogIndex() {
   return (
     <div>
-      <h2>Cree este sitio web provisional para poder empezar a subir mis blogs :)</h2>
-      Me puedes dar feedback en <a href="https://twitter.com/kaitakami_/" target="_blank">twitter</a>
+      <h2>
+        Cree este sitio web provisional para poder empezar a subir mis blogs :)
+      </h2>
+      Me puedes dar feedback en{" "}
+      <a href="https://twitter.com/kaitakami_/" target="_blank">
+        twitter
+      </a>
       <ul>
-      <li>
-          <Link to="reto-blog">Reto: Escribir un blog diario</Link>  
+        <li>
+          <Link to="6">Deje de comer azúcar procesada por un mes</Link>
+        </li>
+        <li>
+          <Link to="5">Los videojuegos nos forman</Link>
+        </li>
+        <li>
+          <Link to="reto-blog">Reto: Escribir un blog diario</Link>
         </li>
         <li>
           <Link to="sistema-de-productividad">Mi sistema de productividad</Link>
         </li>
         <li>
-          <Link to="crear-una-startup-con-17">Crear una startup con 17 años 💻</Link>
+          <Link to="crear-una-startup-con-17">
+            Crear una startup con 17 años 💻
+          </Link>
         </li>
       </ul>
     </div>
